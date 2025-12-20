5290812baf3258ee7ae68fa4ea0dbc4394cdab06
Vigneshwar <r.vigneshwar2000@gmail.com>
2025-12-20 22:33:07 +0530

    refresh

--- src/main/java/com/strengthlog/entity/User.java
+++ src/main/java/com/strengthlog/entity/User.java
@@ -13,13 +13,23 @@ public class User
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false, unique = true)
     private String username;
+    @Column(nullable = false, unique = true)
     private String password;
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
+
     private LocalDateTime createdAt;
+    @PrePersist
+    protected void onCreate()
+    {
+        createdAt = LocalDateTime.now();
+    }
 
 
 }
